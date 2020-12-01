e175b0db7566a3b8ff8fdf626c83d9da80dd54c5
Sanpi <sanpi@homecomputing.fr>
2020-12-01 10:23:28 +0100

    Long display for one package

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use structopt::StructOpt;
 
+type Result<T = ()> = std::result::Result<T, Box<dyn std::error::Error>>;
+
 #[derive(StructOpt)]
 struct Opt {
     dependencies: Vec<String>,
@@ -9,43 +11,26 @@ struct Opt {
     no_dev: bool,
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    use std::io::Write;
-
+fn main() -> Result {
     let opt = Opt::from_args();
     let metadata = cargo_metadata::MetadataCommand::new()
         .manifest_path(&opt.manifest_path)
         .exec()?;
 
-    let mut table = tabwriter::TabWriter::new(Vec::new());
-
     let resolve = metadata.resolve.as_ref().unwrap();
-    let dependencies = dependencies(&metadata, &resolve.root.as_ref().unwrap()).unwrap();
+    let packages = dependencies(&metadata, &resolve.root.as_ref().unwrap())
+        .unwrap()
+        .iter()
+        .filter(|x| !opt.no_dev || !dev_only(x))
+        .map(|x| package(&metadata, &x.pkg).unwrap())
+        .filter(|x| opt.dependencies.is_empty() || opt.dependencies.contains(&x.name))
+        .collect::<Vec<_>>();
 
-    for dependency in dependencies {
-        if opt.no_dev && dev_only(&dependency) {
-            continue;
-        }
-
-        let package = package(&metadata, &dependency.pkg).unwrap();
-
-        if !opt.dependencies.is_empty() && !opt.dependencies.contains(&package.name) {
-            continue;
-        }
-
-        let row = format!(
-            "{}\t{}\t{}\n",
-            package.name,
-            package.version,
-            package.description.as_ref().unwrap_or(&"".to_string())
-        );
-        table.write_all(row.as_bytes())?;
+    if !opt.dependencies.is_empty() && packages.len() == 1 {
+        display_one(&packages[0])
+    } else {
+        display_list(&packages)
     }
-
-    let s = String::from_utf8(table.into_inner()?)?;
-    print!("{}", s);
-
-    Ok(())
 }
 
 fn dependencies<'a>(
@@ -73,4 +58,57 @@ fn dev_only(dependency: &cargo_metadata::NodeDep) -> bool {
     dependency.dep_kinds.iter()
         .map(|x| x.kind)
         .collect::<Vec<_>>() == vec![cargo_metadata::DependencyKind::Development]
+}
+
+fn display_list(packages: &[&cargo_metadata::Package]) -> Result {
+    use std::io::Write;
+
+    let mut table = tabwriter::TabWriter::new(Vec::new());
+
+    for package in packages {
+        let row = format!(
+            "{}\t{}\t{}\n",
+            package.name,
+            package.version,
+            package.description.as_ref().unwrap_or(&"".to_string())
+        );
+        table.write_all(row.as_bytes())?;
+    }
+
+    let s = String::from_utf8(table.into_inner()?)?;
+    print!("{}", s);
+
+    Ok(())
+}
+
+fn display_one(package: &cargo_metadata::Package) -> Result {
+    use std::io::Write;
+
+    let mut table = tabwriter::TabWriter::new(Vec::new());
+
+    table.write_all(&row("name", Some(&package.name)))?;
+    table.write_all(&row("descrip.", package.description.as_ref()))?;
+    table.write_all(&row("keywords", Some(&package.keywords.join(", "))))?;
+    table.write_all(&row("categories", Some(&package.categories.join(", "))))?;
+    table.write_all(&row("version", Some(&package.version)))?;
+    table.write_all(&row("license", package.license.as_ref()))?;
+    table.write_all(&row("homepage", package.homepage.as_ref()))?;
+    table.write_all(&row("repository", package.repository.as_ref()))?;
+    let features = package.features.keys().map(|x| x.clone()).collect::<Vec<_>>();
+    table.write_all(&row("features", Some(&features.join(", "))))?;
+
+    let s = String::from_utf8(table.into_inner()?)?;
+    print!("{}", s);
+
+    Ok(())
+}
+
+fn row<S: ToString>(key: &str, value: Option<&S>) -> Vec<u8> {
+    format!(
+        "{}\t: {}\n",
+        ansi_term::Colour::Green.paint(key),
+        value.map(|x| x.to_string()).unwrap_or_default(),
+    )
+        .as_bytes()
+        .to_vec()
 }
